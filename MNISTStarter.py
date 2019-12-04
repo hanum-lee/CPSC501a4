d58a45b9c5ea99f83026d5b61357900dbd46856d
hanum-lee <chococloudy@gmail.com>
2019-12-04 10:15:45 -0700

    part one done

--- MNISTStarter.py
+++ MNISTStarter.py
@@ -10,19 +10,28 @@ import tensorflow as tf
 print("--Get data--")
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
+x_train = x_train.reshape(x_train.shape[0],28,28,1)
+x_test = x_test.reshape(x_test.shape[0],28,28,1)
 print("--Process data--")
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+input_shape = (28,28,1)
 print("--Make model--")
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(10, activation='sigmoid')
+  tf.keras.layers.Conv2D(28,kernel_size=3,padding='same',activation='relu',input_shape = input_shape),
+  tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+
+  #tf.keras.layers.Flatten(input_shape=(28, 28))
+  #tf.keras.layers.Dense(10, activation='softmax'),
+  
 ])
-model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 print("--Fit model--")
-model.fit(x_train, y_train, epochs=1, verbose=2)
+model.fit(x_train, y_train, epochs=5, verbose=2)
 
 print("--Evaluate model--")
 model_loss, model_acc = model.evaluate(x_test,  y_test, verbose=2)
